d9abb2fc6e4b7ea47a7504758f7711b585aa2979
Eijaz Allibhai <eijaz@Eijazs-MBP.home>
2018-01-30 00:21:59 -0500

    initial commit

---
+++ app/src/main/java/com/example/android/rvtutorial/AnimalAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.android.rvtutorial
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.animal_list_item.view.*
+
+class AnimalAdapter(val items : ArrayList<String>, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
+
+    // Gets the number of animals in the list
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    // Inflates the item views
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.animal_list_item, parent, false))
+    }
+
+    // Binds each animal in the ArrayList to a view
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        holder?.tvAnimalType?.text = items.get(position)
+    }
+}
+
+class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+        // Holds the TextView that will add each animal to
+        val tvAnimalType = view.tv_animal_type
+}
+
---
+++ app/src/main/java/com/example/android/rvtutorial/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.android.rvtutorial
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    // Initializing an empty ArrayList to be filled with animals
+    val animals: ArrayList<String> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Loads animals into the ArrayList
+        addAnimals()
+
+        // Creates a vertical Layout Manager
+        rv_animal_list.layoutManager = LinearLayoutManager(this)
+
+        // You can use GridLayoutManager if you want multiple columns. Enter the number of columns as a parameter.
+//        rv_animal_list.layoutManager = GridLayoutManager(this, 2)
+
+        // Access the RecyclerView Adapter and load the data into it
+        rv_animal_list.adapter = AnimalAdapter(animals, this)
+
+    }
+
+    // Adds animals to the empty animals ArrayList
+    fun addAnimals() {
+        animals.add("dog")
+        animals.add("cat")
+        animals.add("owl")
+        animals.add("cheetah")
+        animals.add("raccoon")
+        animals.add("bird")
+        animals.add("snake")
+        animals.add("lizard")
+        animals.add("hamster")
+        animals.add("bear")
+        animals.add("lion")
+        animals.add("tiger")
+        animals.add("horse")
+        animals.add("frog")
+        animals.add("fish")
+        animals.add("shark")
+        animals.add("turtle")
+        animals.add("elephant")
+        animals.add("cow")
+        animals.add("beaver")
+        animals.add("bison")
+        animals.add("porcupine")
+        animals.add("rat")
+        animals.add("mouse")
+        animals.add("goose")
+        animals.add("deer")
+        animals.add("fox")
+        animals.add("moose")
+        animals.add("buffalo")
+        animals.add("monkey")
+        animals.add("penguin")
+        animals.add("parrot")
+
+    }
+
+}
